2c73fd8f1f2b87c0ee75b75ebf8799e64c1fadb7
kila606 <90197338+kila606@users.noreply.github.com>
2025-11-19 12:18:09 +0800

    11/19 12:17PM

--- For coding pass one desktop/1204.cpp
+++ For coding pass one desktop/1204.cpp
@@ -7,10 +7,19 @@ int main()
 {
 	// Put your code below
 	string months[12] = {"January", "February", "March", "April","May", "June", "July", "August","September", "October", "November", "December"};
-	int year = 0, month = 0;
+	int year = 0, month = 0,date=0;
+	bool x = false; //false 代表負數 true 代表正數
 	
-	while (scanf_s("%4d-%2d", &year,&month))
+	while (scanf_s("%4d-%2d %d", &year,&month,&date))
 	{
+		if (date > 0) x = true;
+		else x = false;
+
+		
+
+
+
+
 		printf("true");
 	}
 
